567c0354cf23a0c94a5a9f3722531dd4591c363e
MarcoKlemenc <marcoklemenc@gmail.com>
2017-06-18 14:50:52 -0300

    Se crea una clase para manejar el formato de las entradas y salidas
    Más específicamente, las conversiones entre texto y hash.

---
+++ eventos/spec/formateador_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require_relative '../model/formateador'
+
+describe 'Formateador' do
+  it 'Deberia dar un texto con formato a partir de un hash' do
+    hash = {a: "b", c: "d"}
+    texto = '{
+  "a": "b",
+  "c": "d"
+}'
+    formateador = Formateador.new
+    expect(formateador.dar_formato(hash)).to eq texto
+  end
+  
+  it 'Deberia dar un hash a partir de un texto con formato' do
+    texto = '{
+  "a": "b",
+  "c": "d"
+}'
+    hash = {"a" => "b", "c" => "d"}
+    formateador = Formateador.new
+    expect(formateador.leer(texto)).to eq hash
+  end
+
+end
