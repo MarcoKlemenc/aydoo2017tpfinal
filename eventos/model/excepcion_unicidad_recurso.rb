ac79fae5c7dfc2a614e00b7fb202d4ad17bfe6b7
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-06-19 23:36:11 -0300

    Se crea excepcion para unicidad de recursos

---
+++ eventos/model/excepcion_unicidad_recurso.rb
@@ -0,0 +1,6 @@
+# Error para violacion de unicidad de recursos segun su nombre
+class ExcepcionUnicidadRecurso < StandardError
+  def initialize(msg = 'Ya existe un recurso con ese nombre.')
+    super
+  end
+end
