4baa6e458a87d5128d61f3e92962ca2804a4a996
rocioechegaraysierra <rocio.echegaray.sierra@gmail.com>
2017-06-20 23:10:58 -0300

    Se cambia mensaje de ExcepcionSolapamientoRecurso

--- eventos/model/excepcion_solapamiento_recurso.rb
+++ eventos/model/excepcion_solapamiento_recurso.rb
@@ -1,7 +1,7 @@
 # Error para cuando se solapan los intervalos
 # de tiempo entre recursos de un evento
 class ExcepcionSolapamientoRecurso < StandardError
-  def initialize(msg = 'No se pueden solapar recursos en un mismo evento.')
+  def initialize(msg = 'No se pueden solapar eventos en un mismo recurso.')
     super
   end
 end
